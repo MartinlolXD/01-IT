3bc533a38f0df9168c1eefe7125d6b2111c68ed9
MartinlolXD <145441445+MartinlolXD@users.noreply.github.com>
2023-10-23 15:03:12 +0200

    NEVIM CO TO MÁM UŽ PSÁT

--- Kalkulačka/script.js
+++ Kalkulačka/script.js
@@ -12,3 +12,33 @@ else if (inputNumber == 10) {
 else {
     console.log ("Nesprávný znak")
 }
+
+
+
+/// Pole (array)
+
+const classMates = ["FrantaBaroch", "Tomaš", "FrantaŠestách"]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const
+
+const cM2 = [];
+cM2[0]="FrantaBaroch"
+cM2[1]="FrantaŠestách"
+cM2[2]="TomašOubrecht"
+cM2[3]="PetrSýkora"
+cM2[4]="AdamŠvarc"
+cM2[5]="KristiánBotek"
+cM2[6]="Tony"
+cM2[7]="HonzaGrás"
+cM2[8]="RudolfPaul"
+cM2[9]="MichalBezděk"
+cM2[10]="LukášŠifalda"
+
+console.log (cM2) // NE!
+
+
+function forLoop() {
+    for(let i =0; i < cM2.length i++){
+    console.log("cM2[i]")
+  }
+}
