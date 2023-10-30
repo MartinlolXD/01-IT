ac852755c01be4d73ecf0d82c9e5007b1837e957
MartinlolXD <145441445+MartinlolXD@users.noreply.github.com>
2023-10-30 14:56:42 +0100

    Na teams

---
+++ Kontrola věku - Teams/script.js
@@ -0,0 +1,46 @@
+function checkResult() {
+    const inputNumber = (document.getElementById("numberCheck").value)
+    if(inputNumber > 2005) {
+        console.log("Dětský uživatel")
+    }else if (inputNumber < 2005 ) {
+        console.log("Dospělý uživatel")
+    }
+    else if (inputNumber == 2005) {
+        console.log("Dospělý uživatel")
+    }
+} 
+
+function resultCheck() {
+    const inputNumber = (document.getElementById("checkNumber").value)
+    if(inputNumber < 18) {
+        console.log("Dětský uživatel")
+    }else if (inputNumber < 17 ) {
+        console.log("Dospělý uživatel")
+    }
+    else if (inputNumber == 18) {
+        console.log("Dospělý uživatel")
+    }
+} 
+
+function resultOfCheck() {
+    const inputNumber = (document.getElementById("checkNumber").value)
+    
+    if(inputNumber < 18) {
+        console.log("Dětský uživatel")
+    }else if (inputNumber < 17 ) {
+        console.log("Dospělý uživatel")
+    }
+    else if (inputNumber == 18) {
+        console.log("Dospělý uživatel")
+    }
+var numberInput = (document.getElementById("checkNumber").value)
+    if(numberInput > 2005) {
+        console.log("Dětský uživatel")
+    }else if (numberInput < 2005 ) {
+        console.log("Dospělý uživatel")
+    }
+    else if (numberInput == 2005) {
+        console.log("Dospělý uživatel")
+    }
+    
+} 
